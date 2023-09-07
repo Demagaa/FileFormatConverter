01f69f72135205c6cf90a4ee97f59ffd96362100
achursin <achursin@mit-consulting.cz>
2023-09-07 14:31:47 +0200

    Final commit

--- sctipts/excel_processing.py
+++ sctipts/excel_processing.py
@@ -1,66 +1,147 @@
+import re
 import pandas as pd
 
 # Load the Excel file
-input_file = 'C:\\Users\\lepro\\IdeaProjects\\FIleFormatConverter\\input_data\\Formatova_pravidla_02_test.xlsx'
-csv_output_file = 'output.xlsx'
+input_file = 'C:\\Users\\lepro\\IdeaProjects\\FIleFormatConverter\\input_data\\test_input.xlsx'
+
+# Specify the desired file name (including the ".csv" extension)
+file_name = 'output.csv'
+
+# Specify the full path to the directory where you want to save the CSV file
+directory_path = 'C:\\Users\\lepro\\IdeaProjects\\FIleFormatConverter\\output_data\\'
 
 # Read the Excel file into a pandas DataFrame
 df = pd.read_excel(input_file)
 
+# Rename columns for clarity
+df = df.rename(columns={
+    'Verze': 'outputDataFormat',
+    'MIMETYPE': 'containerDataFormat',
+    'Název': 'name',
+    'Kategorie': 'category',
+    'Přípona': 'extension',
+    'Výstupní formát': 'outPuid',
+    'Originál vždy (doporučeno)': 'keepOriginal',
+    'Výstupní formát alternativně I': 'possibleOutPuid'
+})
+
+# Dictionaries for replacements
 number_dict = {
     '2': 'fmt/477',
     '3': 'fmt/645,fmt/10,fmt/13',
     '4': 'fmt/4,fmt/649,fmt/640,fmt/199',
     '5': 'fmt/134,fmt/198,fmt/141',
-    '6': 'fmt/101',
-    'rozbalit (komponenty dle formátu)': df['PUID']
+    '6': 'fmt/101'
+}
+
+bold_number_dict = {
+    '2': 'fmt/477',
+    '3': 'fmt/645',
+    '4': 'fmt/4',
+    '5': 'fmt/134',
+    '6': 'fmt/101'
 }
 
 exception_dict = {
     'ponechat': df['PUID'],
     'výstupní': df['PUID'],
-    'individuální': df['PUID'],
-}
-
-bool_dict = {
-    'ano': 'true',
-    '': 'false'
+    'individuální': df['PUID']
 }
 
 
-def replace_puid(input_str, row):
-    parts = input_str.split()  # Split by commas
-    modified_parts = []
+# Function to perform replacements
+def replace(row):
+    input_str = row['outPuid']
+    modified_parts_possible_out_puid = []
+    modified_parts_out_puid = []
+    modified_parts_output = []
+    modified_parts_container = []
 
-    for part in parts:
-        # Extract the number (e.g., '2' from '§ 23 odst. 2')
-        word = part.strip().split()[-1]
-        # Replace the number with the corresponding value from replacement_dict
-        if word in number_dict:
-            modified_parts.append(number_dict[word])
-        if word in exception_dict:
-            modified_parts.append(number_dict[word])
-            row['Verze'] = True
+    try:
+        # Try to convert input_str to float; if successful, return a predefined value
+        float(input_str)
+        return f"{row['PUID']}###{row['PUID']}###{'false'}###{'false'}"
 
-    # Concatenate the modified parts with commas
-    result = ','.join(modified_parts)
+    except ValueError:
+        # Split the input string into parts
+        parts = re.findall(r'\d+|\w+', input_str, re.IGNORECASE)
 
-    return result
+        for part in parts:
+            # Extract the number (e.g., '2' from '§ 23 odst. 2')
+            word = part.strip().split()[-1]
+
+            # Replace the number with the corresponding value from dictionaries
+            if word in number_dict:
+                modified_parts_possible_out_puid.append(number_dict[word])
+                if len(modified_parts_container) < 1:
+                    modified_parts_out_puid.append(bold_number_dict[word])
+                    modified_parts_output.append('false')
+                    modified_parts_container.append('false')
+
+            elif word in exception_dict:
+                modified_parts_possible_out_puid.append(row['PUID'])
+                modified_parts_out_puid.append(row['PUID'])
+                modified_parts_output.append('true')
+                modified_parts_container.append('false')
+
+            elif word == 'rozbalit':
+                modified_parts_possible_out_puid.append(row['PUID'])
+                modified_parts_out_puid.append(row['PUID'])
+                modified_parts_output.append('false')
+                modified_parts_container.append('true')
+
+        # Concatenate the modified parts with commas
+        result_out_puid = ','.join(modified_parts_out_puid)
+        result_output = ','.join(modified_parts_output)
+        result_container = ','.join(modified_parts_container)
+        result_possible_out_puid = ','.join(modified_parts_possible_out_puid)
+
+        # Combine values with a delimiter
+        combined_result = f"{result_out_puid}###{result_possible_out_puid}###{result_output}###{result_container}"
+
+        return combined_result
 
 
+# Function to replace boolean values
 def replace_bool(input_str):
-    if input_str in number_dict:
-        return number_dict[input_str]
+    if input_str == 'ano':
+        return 'true'
+    else:
+        return 'false'
 
 
-# Change values in column 'A' based on previous values
-df['Výstupní formát'] = df['Výstupní formát'].apply(replace_puid, df['Verze'])
-df['L'] = df['L'].apply(replace_bool)
+# Define a function to split the combined string by '###'
+def custom_split(combined_str):
+    # Split the combined string by '###'
+    parts = combined_str.split('###')
+
+    # Ensure that there are at least tree parts
+    if len(parts) >= 4:
+        return parts[0], parts[1], parts[2], parts[3]
+    else:
+        return parts[0], None
+
+
+# Apply the replace function to each row and store the result in a new column
+df['Combined'] = df.apply(replace, axis=1)
+
+# Split the 'Combined' column into 'outPuid', 'possibleOutPuid', 'outputDataFormat' and 'containerDataFormat' columns
+df[['outPuid',
+    'possibleOutPuid',
+    'outputDataFormat',
+    'containerDataFormat']] = df['Combined'].apply(custom_split).apply(pd.Series)
+
+# Change values in column 'keepOriginal' based on previous values
+df['keepOriginal'] = df['keepOriginal'].apply(replace_bool)
 
 # Remove unnecessary columns
-# df = df.drop(columns=['A', 'D', 'F', 'I', 'J', 'K', 'M'])
+df = df.drop(columns=['id',
+                      'Výstupní formát alternativně II',
+                      'Výstupní formát alternativně III',
+                      'Komentář',
+                      'Combined'])
 
 # Save the DataFrame as a CSV file
-df.to_csv(csv_output_file, index=False)
+df.to_excel(directory_path + file_name, index=False)
 
-print("Output file has been converted to CSV and saved as", csv_output_file)
+print("Output file has been converted to CSV and saved as", file_name)
